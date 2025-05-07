0bb8cfe8ff2f84417946e198527c3d58c8c8e3d9
Faizul Haque <42814538+fisiaque@users.noreply.github.com>
2025-05-07 19:16:10 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,3 @@
-if shared.Init then return end
-shared.Init = true;
-
 -- variable
 local marked = "--MARKED: DELETE IF CACHED INCASE BAYA UPDATES.\n"
 local hubFolders = {"Baya/Hub", "Baya/Hub/Games"}
