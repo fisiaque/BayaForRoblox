e71bccc23879992489cf22a846f7bcec2edf139c
fisiaque <fisiaque@gmail.com>
2025-05-07 01:18:45 +0100

    + Modules added

--- Hub/Games/universal.lua
+++ Hub/Games/universal.lua
@@ -1,5 +1,23 @@
-shared.library:CreateCategory({
+local test0 = shared.library:CreateCategory({
 	Name = "Test0",
  	Icon = "Baya/UIAssets/ActionIcon.png",
  	Size = UDim2.fromOffset(13, 14)
 });
+
+local hello = test0:CreateModule({
+	Name = 'Hello World',
+})
+
+hello:CreateButton({Name = 'sh00t',
+	Function = function(callback)
+		print("BAM BAM BAM")
+	end,
+	Tooltip = '!!!'
+})
+
+hello:CreateButton({Name = 'Auto Boom',
+	Function = function(callback)
+		print("gg ff gg")
+	end,
+	Tooltip = "Sdawdsadwadswwwwwwwwwwwwwwwwwwwwwwwwwww"
+})
